a887a280e864f9787cd5038f2cc4d9ec84e7145b
Pavel <88451334+PavelEpanov@users.noreply.github.com>
2022-09-15 16:17:39 +0500

    Add files via upload

---
+++ umnozh.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+int main(){
+    int v1 = 0;
+    int v2 = 0;
+    std::cout << "Okey try ur numbers: " << std::endl;
+    std::cin >> v1; // OKey it is #####
+    std::cin >> v2;
+    int answer = v1 * v2;
+    std::cout << "Answer is: " << answer << std::endl;
+    std::cout << "Answer" << std::endl;
+    std::cout << "is" << std::endl;
+    std::cout << answer << std::endl;
+    return 0;
+}
